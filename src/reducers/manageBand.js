8e8f6faa4fff23f5cc2742d6b488d19aa2cb4cd5
Patrick Brennan <pbrennanmusic@gmail.com>
2021-12-23 19:38:05 -0500

    commit tree 81d392b8949faa363349aaeb7148f0eec9a05c7c

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,11 +1,20 @@
+import uuid from 'uuid';
+
 export default function manageBand(state = {
   bands: [],
 }, action) {
   switch(action.type) {
     case "ADD_BAND":
-      return {...state, bands: state.bands.concat(action.name)}
+      const band = {
+        id: uuid(),
+        name: action.name.name
+      }
+      return {...state, bands: state.bands.concat(band)}
+
+    case 'DELETE_BAND':
+      return {...state, bands: state.bands.filter(band => band.id !== action.id)}
     default:
       return state
-  
+
   }
 };
